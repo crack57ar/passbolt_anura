93920c15b0bf32dcdeadb67ca4fb9e2c3dda6ac9
Miguel Valdez <crack57ar@gmail.com>
2017-10-12 17:54:45 -0300

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ ! -e docker-compose ]; then
-	curl -L https://github.com/docker/compose/releases/download/1.3.2/docker-compose-`uname -s`-`uname -m` > docker-compose
-fi
-chmod +x docker-compose
-./docker-compose up -d
